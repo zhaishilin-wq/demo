fea7407cc20c62fc21dd2a4ee0b082d764c2a15a
翟仕林 <7551594+zhai_shilin@user.noreply.gitee.com>
2024-06-25 10:51:10 +0800

    修改

--- src/main/java/com/example/demo/service/impl/TestImp.java
+++ src/main/java/com/example/demo/service/impl/TestImp.java
@@ -3,4 +3,13 @@ package com.example.demo.service.impl;
 import com.example.demo.service.Test;
 
 public class TestImp implements Test {
+    public static void main(String[] args) {
+        int x = 10;
+        while( x < 20 ) {
+            System.out.print("value of x : " + x );
+            x++;
+            System.out.print("\n");
+        }
+    }
+
 }
