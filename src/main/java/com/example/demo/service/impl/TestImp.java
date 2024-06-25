2a2718fc8ee8b0c705389f757dd776fb7279c994
翟仕林 <7551594+zhai_shilin@user.noreply.gitee.com>
2024-06-25 11:32:39 +0800

    修改

--- src/main/java/com/example/demo/service/impl/TestImp.java
+++ src/main/java/com/example/demo/service/impl/TestImp.java
@@ -6,7 +6,7 @@ public class TestImp implements Test {
     public static void main(String[] args) {
         int x = 10;
         while( x < 20 ) {
-            System.out.print("value of x : " + x );
+            System.out.print("value of x :: " + x );
             x++;
             System.out.print("\n");
         }
